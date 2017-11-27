9d60000af834963e9859c74d7c0c9a48407e88ab
eranlaudin <eraner21@gmail.com>
2017-11-27 13:00:51 +0200

    Database start

--- Utils/Database/DBHelper.py
+++ Utils/Database/DBHelper.py
@@ -12,8 +12,6 @@ sql_command = """CREATE TABLE employee
 (staff_number INTEGER PRIMARY KEY,fname VARCHAR(20),lname VARCHAR(30),
 gender CHAR(1),joining DATE,birth_date DATE);"""
 
-
-
 cursor.execute(sql_command)
 
 connection.commit()
