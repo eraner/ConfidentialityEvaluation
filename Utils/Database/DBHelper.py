ba7d38cb9a691c72872f62732b78ce0075a5e194
eranlaudin <eraner21@gmail.com>
2017-12-11 18:11:29 +0200

    Added init resources

--- Utils/Database/DBHelper.py
+++ Utils/Database/DBHelper.py
@@ -1,5 +1,9 @@
 import sqlite3
 
+"""
+TODO: documentation
+"""
+
 
 def execute_query(sql_command):
     '''
@@ -17,27 +21,64 @@ def db_init():
     Initialize Database.
     :return:
     '''
-    # delete
+# delete
     execute_query("""DROP TABLE Users;""")
 
-    # Create Users table
+# Create Users table
     sql_command = """CREATE TABLE Users
                     (id INTEGER PRIMARY KEY,
-                        name VARCHAR(40),
-                        role VARCHAR(20),
-                        joining DATE,
-                        birth_date DATE);"""
+                    u_name VARCHAR(40),
+                    role VARCHAR(40),
+                    joining DATE,
+                    birth_date DATE,
+                    FOREIGN KEY(role) REFERENCES Roles(name)
+                    );"""
     execute_query(sql_command)
 
-    # Add Users
+# Add Users
     sql_command = """INSERT INTO Users (id, name, role, joining, birth_date)
-        VALUES (NULL, "Eran Laudin", "Manager", "2017-11-02", "1961-10-25");"""
-    execute_query(sql_command)
-    sql_command = """INSERT INTO Users (id, name, role, joining, birth_date)
-        VALUES (NULL, "Ohad Cohen", "Cleaner", "2017-11-02", "1965-05-20");"""
+        VALUES (NULL, "Eran Laudin", "Manager", "2017-11-02", "1961-10-25"),
+                (NULL, "Ohad Cohen", "Cleaner", "2017-11-02", "1965-05-20");"""
     execute_query(sql_command)
 
-    # Create 
+# Create Roles table
+    sql_command = """CREATE TABLE Roles
+                    (name INTEGER PRIMARY KEY,
+                    rank INTEGER
+                    );"""
+    execute_query(sql_command)
+
+# Add roles
+    sql_command = """INSERT INTO Roles (name, rank) VALUES
+                    ("Manager", 1),
+                    ("Developer", 3),
+                    ("QA", 4),
+                    ("Team Leader", 2),
+                    ("Cleaner", 5),
+                    ("Guard", 4);"""
+    execute_query(sql_command)
+
+
+# Create Resources table
+    sql_command = """CREATE TABLE Resources
+                    (id INTEGER PRIMARY KEY,
+                    name VARCHAR(20),
+                    type VARCHAR(20)
+                    );"""
+    execute_query(sql_command)
+
+# Add Resources
+    sql_command = """INSERT INTO Resources (id, name, type) VALUES
+                    (NULL, "DevCode", "dir"),
+                    (NULL, "Salaries", "xlsx"),
+                    (NULL, "", "");
+                    """
+
+# Create Rules table
+    sql_command = """CREATE TABLE Rules
+                    (TODO
+                    );"""
+    execute_query(sql_command)
 
 if __name__ == "__main__":
     connection = sqlite3.connect("auth.db")
